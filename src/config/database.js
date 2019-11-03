273837e60bd2aa246236965f1547b904f67f8ed2
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-03 17:40:05 -0300

    Setting the database

---
+++ src/config/database.js
@@ -0,0 +1,26 @@
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'docker',
+    database: 'gympoint',
+    host: '172.17.0.2',
+    dialect: 'postgres',
+    operatorsAliases: false
+  },
+  test: {
+    username: 'postgres',
+    password: 'docker',
+    database: 'gympoint_test',
+    host: '172.17.0.2',
+    dialect: 'postgres',
+    operatorsAliases: false
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false
+  }
+};
