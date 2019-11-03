fd3d5749117e284aac3a0a3e5945a0cff28d3470
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-03 18:16:10 -0300

    Fix `DeprecationWarning` from database config

--- src/config/database.js
+++ src/config/database.js
@@ -4,23 +4,20 @@ module.exports = {
     password: 'docker',
     database: 'gympoint',
     host: '172.17.0.2',
-    dialect: 'postgres',
-    operatorsAliases: false
+    dialect: 'postgres'
   },
   test: {
     username: 'postgres',
     password: 'docker',
     database: 'gympoint_test',
     host: '172.17.0.2',
-    dialect: 'postgres',
-    operatorsAliases: false
+    dialect: 'postgres'
   },
   production: {
     username: 'root',
     password: null,
     database: 'database_production',
     host: '127.0.0.1',
-    dialect: 'mysql',
-    operatorsAliases: false
+    dialect: 'mysql'
   }
 };
