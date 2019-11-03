5c5aa1364fab79a4609b1d022912d90b364ba178
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-03 20:45:32 -0300

    Adding jwt on project

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,6 +7,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING
       },
       {
@@ -14,6 +16,10 @@ class User extends Model {
     );
 
     return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '5d33e472a9e341a891be1f800401a4df',
+  expiresIn: '7d'
+};
