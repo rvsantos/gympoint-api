92edcef4a55e4525162271b0273d2b0984cf8003
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-04 21:59:25 -0300

    Authentication for register students

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,14 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 
+import authMiddlewares from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddlewares);
+
 routes.post('/students', StudentController.store);
 
 export default routes;
