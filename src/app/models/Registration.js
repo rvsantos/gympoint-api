1937a04bd7c186af8171d987d0026add7e9b0aed
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-13 18:48:28 -0300

    Change type of price to decimal

--- src/app/models/Registration.js
+++ src/app/models/Registration.js
@@ -6,7 +6,7 @@ class Registration extends Model {
       {
         start_date: Sequelize.DATE,
         end_date: Sequelize.DATE,
-        price: Sequelize.DOUBLE
+        price: Sequelize.DECIMAL
       },
       {
         sequelize
@@ -17,8 +17,10 @@ class Registration extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.Student, { foreignKey: 'student_id', as: 'student' });
-    this.belongsTo(models.Plan, { foreignKey: 'plan_id', as: 'plan' });
+    this.belongsTo(models.Student, {
+      foreignKey: 'student_id'
+    });
+    this.belongsTo(models.Plan, { foreignKey: 'plan_id' });
   }
 }
 
