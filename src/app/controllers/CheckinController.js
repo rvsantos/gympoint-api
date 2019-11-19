48536b2d01dc013e3e3d7e1fe03c77ef87b91f54
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-19 13:41:54 -0300

    Removing Yup from CheckinController

--- src/app/controllers/CheckinController.js
+++ src/app/controllers/CheckinController.js
@@ -1,17 +1,8 @@
-import * as Yup from 'yup';
 import Checkin from '../models/Checkin';
 import Student from '../models/Student';
 
 class CheckinController {
   async store(req, res) {
-    // const schema = Yup.object().shape({
-    //   student_id: Yup.number().positive()
-    // });
-
-    // if (!(await schema.isValid(req.body))) {
-    //   return res.status(400).json({ errors: 'Validation fails' });
-    // }
-
     const { student_id } = req.params;
 
     const student = await Student.findByPk(student_id);
