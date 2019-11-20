a600cdd125b53cdd58aa17d5282483c41b74129b
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-20 01:04:02 -0300

    Adding answer job for send email when question is respond

--- src/app/controllers/AnswerController.js
+++ src/app/controllers/AnswerController.js
@@ -1,5 +1,9 @@
 import * as Yup from 'yup';
 import HelpOrder from '../models/HelpOrder';
+import Student from '../models/Student';
+
+import AnswerMail from '../jobs/AnswerMail';
+import Queue from '../../lib/Queue';
 
 class AnswerController {
   async store(req, res) {
@@ -14,10 +18,23 @@ class AnswerController {
       return res.status(400).json({ errors: 'Validation fails' });
     }
 
-    const helpOrder = await HelpOrder.findByPk(id);
+    const helpOrder = await HelpOrder.findByPk(id, {
+      attributes: ['id', 'question', 'answer', 'answer_at', 'created_at'],
+      include: [
+        {
+          model: Student,
+          attributes: ['name', 'email']
+        }
+      ]
+    });
+
     helpOrder.answer = answer;
     helpOrder.answer_at = new Date();
     await helpOrder.save();
+
+    await Queue.add(AnswerMail.key, {
+      helpOrder
+    });
 
     return res.json(helpOrder);
   }
