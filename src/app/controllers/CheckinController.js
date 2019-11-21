cd96c40f1b02101bf7563226697fd5b4a381d725
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-20 23:49:13 -0300

    Limiting checkin to 5 in 7 days

--- src/app/controllers/CheckinController.js
+++ src/app/controllers/CheckinController.js
@@ -1,3 +1,5 @@
+import { startOfWeek, endOfWeek } from 'date-fns';
+import { Op } from 'sequelize';
 import Checkin from '../models/Checkin';
 import Student from '../models/Student';
 
@@ -27,6 +29,24 @@ class CheckinController {
 
     if (!student) {
       return res.status(422).json({ error: 'Student does not found' });
+    }
+
+    const startWeek = startOfWeek(new Date());
+    const endWeek = endOfWeek(new Date());
+
+    const countCheckins = await Checkin.findAndCountAll({
+      where: {
+        [Op.and]: {
+          created_at: {
+            [Op.between]: [startWeek, endWeek]
+          },
+          student_id
+        }
+      }
+    });
+
+    if (countCheckins.count >= 5) {
+      return res.status(400).json({ error: 'Limit checkins is 5' });
     }
 
     const studentCheckin = await Checkin.create({
