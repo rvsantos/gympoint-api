70cd88bb5d111737932894a23f238d9695385cfd
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-20 23:50:15 -0300

    Adding exception handler

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import 'dotenv/config';
 import express from 'express';
+import Youch from 'youch';
+import 'express-async-errors';
+
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
 
 import routes from './routes';
 
@@ -9,16 +14,29 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middleware();
     this.routes();
+    this.excepetionHandler();
   }
 
   middleware() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  excepetionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://3c18d7d583ef41469328f597a68c465c@sentry.io/1828421'
+};
